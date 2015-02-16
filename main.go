4d078a5b07265b1e9cb8053a66ef292a0f9e428b
Tomotaka Sakuma <ktmtmks@gmail.com>
2015-02-17 08:40:04 +0900

    bump up to 0.1.2

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "librarian-puppet-go"
-	app.Version = "0.1.1"
+	app.Version = "0.1.2"
 	app.Flags = []cli.Flag{
 		cli.BoolFlag{Name: "verbose", Usage: "Show logs verbosely"},
 	}
