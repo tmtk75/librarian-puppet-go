e558f110c8f766cc6279fac87aff1cd30dd3841e
Tomotaka Sakuma <ktmtmks@gmail.com>
2016-06-24 12:29:34 +0900

    Fix bumpUpMod

--- bump-up.go
+++ bump-up.go
@@ -14,38 +14,72 @@ func bumpUp(a, b string, init string) {
 	}
 }
 
-func bumpUpMod(n Mod, mods []Mod, rel, filename string, diff func(wd, a, b string) string) string {
+type bumpDiff func(wd, a, b string) string
+
+func bumpUpMod(n Mod, mods []Mod, rel, filename string, diff bumpDiff) (string, error) {
+	g := NewGit()
+	g.Diff = diff
+	return g.bumpUpMod(n, mods, rel, filename)
+}
+
+func (g *Git) bumpUpMod(n Mod, mods []Mod, rel, filename string) (string, error) {
 	m, err := findModIn(mods, n)
 	if err != nil {
 		logger.Printf("INFO: %v in %v. old one is used\n", err, filename)
 		if n.opts["ref"] != "" {
 			opts := map[string]string{"git": n.opts["git"], "ref": rel}
 			p := Mod{name: n.name, version: n.version, opts: opts, user: n.user}
-			return p.Format()
+			return p.Format(), nil
 		} else {
-			return n.Format()
+			return n.Format(), nil
 		}
 	}
 	aref := m.opts["ref"]
 	bref := n.opts["ref"]
 	if aref == "" || bref == "" {
-		return n.Format()
+		return n.Format(), nil
 	}
 
-	d := diff(n.Dest(), aref, bref)
+	if g.IsTag(m.Dest(), aref) && g.IsBranch(n.Dest(), bref) {
+		d := g.Diff(n.Dest(), aref, bref)
+		if d == "" {
+			opts := map[string]string{"git": n.opts["git"], "ref": m.opts["ref"]}
+			p := Mod{name: n.name, version: n.version, opts: opts}
+			return p.Format(), nil
+		}
+
+		v, err := minorVersionNumber(bref)
+		if err != nil {
+			return "", err
+		}
+		_, b, c, err := semanticVersion(aref)
+		if err != nil {
+			return "", err
+		}
+		if v > b {
+			r := fmt.Sprintf("v0.%v.0", v)
+			opts := map[string]string{"git": n.opts["git"], "ref": r}
+			return Mod{name: n.name, version: n.version, opts: opts}.Format(), nil
+		}
+		r := fmt.Sprintf("v0.%v.%v", b, c+1)
+		opts := map[string]string{"git": n.opts["git"], "ref": r}
+		return Mod{name: n.name, version: n.version, opts: opts}.Format(), nil
+	}
+
+	d := g.Diff(n.Dest(), aref, bref)
 	if d == "" {
 		opts := map[string]string{"git": n.opts["git"], "ref": m.opts["ref"]}
 		p := Mod{name: n.name, version: n.version, opts: opts}
-		return p.Format()
+		return p.Format(), nil
 	}
 
 	newref, err := increment(m.opts["ref"])
 	if err != nil {
 		log.Printf("INFO: %v for %v. new one is used as is\n", err, n.name)
-		return n.Format()
+		return n.Format(), nil
 	}
 
 	opts := map[string]string{"git": n.opts["git"], "ref": newref}
 	p := Mod{name: n.name, version: n.version, opts: opts}
-	return p.Format()
+	return p.Format(), nil
 }
--- bump-up_test.go
+++ bump-up_test.go
@@ -7,36 +7,96 @@ import (
 )
 
 func TestBumpUpMod(t *testing.T) {
-	nodiff := func(wd, a, b string) string { return "" }
-	diff := func(wd, a, b string) string { return "a" }
+	var nodiff = func(wd, a, b string) string { return "" }
+	var diff = func(wd, a, b string) string { return "a" }
+	isBranch := func(wd, name string) bool {
+		if name == "release/0.2" || name == "release/foobar" {
+			return true
+		}
+		return false
+	}
+	isTag := func(wd, name string) bool {
+		if name == "v0.1.3" || name == "v0.2.1" {
+			return true
+		}
+		return false
+	}
 
-	a, _ := parsePuppetfile(r(``))
-	b, _ := parsePuppetfile(r(`mod 'dprince/qpid'`))
-	e := bumpUpMod(b[0], a, "foo", "a-file", nodiff)
-	assert.Equal(t, "mod 'dprince/qpid'", e)
+	tests := []struct {
+		rel  string
+		diff bumpDiff
+		err  bool
+		src  string
+		dst  string
+		exp  string
+	}{
+		//
+		{
+			"", nodiff, false,
+			``,
+			`mod 'dprince/qpid'`,
+			`mod 'dprince/qpid'`},
+		{
+			"initial", nodiff, false,
+			``,
+			`mod 'fiz/buz', :git => 'abc', :ref => '01234'`,
+			`mod 'fiz/buz', :git => 'abc', :ref => 'initial'`},
+		{
+			"", diff, false,
+			`mod 'a/b', :git => 'aaa'`,
+			`mod 'a/b', :git => 'aaa', :ref => 'development'`,
+			`mod 'a/b', :git => 'aaa', :ref => 'development'`},
+		{
+			"", nodiff, false,
+			`mod 'a/b', :git => 'aaa', :ref => 'release/0.1'`,
+			`mod 'a/b', :git => 'aaa', :ref => 'development'`,
+			`mod 'a/b', :git => 'aaa', :ref => 'release/0.1'`},
+		{
+			"", nodiff, false,
+			`mod 'a/b', :git => 'aaa', :ref => '0123456789a'`,
+			`mod 'a/b', :git => 'aaa', :ref => 'development'`,
+			`mod 'a/b', :git => 'aaa', :ref => '0123456789a'`},
+		{
+			"", diff, false,
+			`mod 'a/b', :git => 'aaa', :ref => 'release/0.1'`,
+			`mod 'a/b', :git => 'aaa', :ref => 'development'`,
+			`mod 'a/b', :git => 'aaa', :ref => 'release/0.2'`},
+		//
+		{
+			"", diff, false,
+			`mod 'a/b', :git => 'aaa', :ref => 'v0.1.3'`,
+			`mod 'a/b', :git => 'aaa', :ref => 'release/0.2'`,
+			`mod 'a/b', :git => 'aaa', :ref => 'v0.2.0'`},
+		{
+			"", nodiff, false,
+			`mod 'a/b', :git => 'aaa', :ref => 'v0.2.1'`,
+			`mod 'a/b', :git => 'aaa', :ref => 'release/0.2'`,
+			`mod 'a/b', :git => 'aaa', :ref => 'v0.2.1'`},
+		{
+			"", diff, false,
+			`mod 'a/b', :git => 'aaa', :ref => 'v0.2.1'`,
+			`mod 'a/b', :git => 'aaa', :ref => 'release/0.2'`,
+			`mod 'a/b', :git => 'aaa', :ref => 'v0.2.2'`},
+		{
+			"", diff, true,
+			`mod 'a/b', :git => 'aaa', :ref => 'v0.2.1'`,
+			`mod 'a/b', :git => 'aaa', :ref => 'release/foobar'`,
+			``},
+	}
 
-	a, _ = parsePuppetfile(r(``))
-	b, _ = parsePuppetfile(r(`mod 'fiz/buz', :git => 'abc', :ref => '01234'`))
-	e = bumpUpMod(b[0], a, "initial", "a-file", nodiff)
-	assert.Equal(t, "mod 'fiz/buz', :git => 'abc', :ref => 'initial'", e)
-
-	a, _ = parsePuppetfile(r(`mod 'a/b', :git => 'aaa'`))
-	b, _ = parsePuppetfile(r(`mod 'a/b', :git => 'aaa', :ref => 'development'`))
-	e = bumpUpMod(b[0], a, "foo", "a-file", diff)
-	assert.Equal(t, "mod 'a/b', :git => 'aaa', :ref => 'development'", e)
-
-	a, _ = parsePuppetfile(r(`mod 'a/b', :git => 'aaa', :ref => 'release/0.1'`))
-	b, _ = parsePuppetfile(r(`mod 'a/b', :git => 'aaa', :ref => 'development'`))
-	e = bumpUpMod(b[0], a, "foo", "a-file", nodiff)
-	assert.Equal(t, "mod 'a/b', :git => 'aaa', :ref => 'release/0.1'", e)
-
-	a, _ = parsePuppetfile(r(`mod 'a/b', :git => 'aaa', :ref => '0123456789a'`))
-	b, _ = parsePuppetfile(r(`mod 'a/b', :git => 'aaa', :ref => 'development'`))
-	e = bumpUpMod(b[0], a, "foo", "a-file", nodiff)
-	assert.Equal(t, "mod 'a/b', :git => 'aaa', :ref => '0123456789a'", e)
-
-	a, _ = parsePuppetfile(r(`mod 'a/b', :git => 'aaa', :ref => 'release/0.1'`))
-	b, _ = parsePuppetfile(r(`mod 'a/b', :git => 'aaa', :ref => 'development'`))
-	e = bumpUpMod(b[0], a, "foo", "a-file", diff)
-	assert.Equal(t, "mod 'a/b', :git => 'aaa', :ref => 'release/0.2'", e)
+	for _, c := range tests {
+		a, _ := parsePuppetfile(r(c.src))
+		b, _ := parsePuppetfile(r(c.dst))
+		g := NewGit()
+		g.Diff = c.diff
+		g.IsTag = isTag
+		g.IsBranch = isBranch
+		e, err := g.bumpUpMod(b[0], a, c.rel, "a-file")
+		if c.err {
+			assert.NotNil(t, err)
+		} else {
+			assert.Nil(t, err)
+		}
+		assert.Equal(t, c.exp, e)
+	}
 }
